85437af72e9572ed48bad2f63557f58f6535c56b
julio cesar <cesar@emesa.com.mx>
2023-10-12 15:38:26 -0600

    primer commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<link rel="stylesheet" href="css/styles.css">
+<body>
+    <div>
+        <img id="logo-head" src="images/logo-tec-a-negocios.svg" alt="">
+    </div>
+</body>
+</html>
